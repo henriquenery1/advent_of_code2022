68cf99c95579ac4d75e7e5968416c85780fa99cd
Henrique Nery <henriquenery07@gmail.com>
2022-12-08 12:41:56 -0300

    feat: code of par2

--- day1/day1.py
+++ day1/day1.py
@@ -12,4 +12,26 @@ def part1():
             current_results = 0
     return print(max_result)
 
+def part2():
+    with open('input.txt', 'r') as f:
+        lines = f.readlines()
+
+    one, two, three = 0, 0, 0
+    current_results = 0
+    for i in lines:
+        if i != '\n':
+            current_results += int(i)
+        else:
+            if current_results > three:
+                if current_results < two:
+                    three = current_results
+                elif current_results < one:
+                    two, three = current_results, two
+                else:
+                    one, two, three = current_results, one, two
+            current_results = 0
+
+    return print(sum([one, two, three]))
+
 part1()
+part2()
